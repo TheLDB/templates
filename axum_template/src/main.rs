e2a0df2d6b5e0c0361bf1d8f577a89a1b7d1fa07
TheLDB <ldb@erikboles.com>
2022-09-01 21:31:13 -0600

    Init

---
+++ axum_template/src/main.rs
@@ -0,0 +1,19 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+pub mod routes;
+use routes::handler::handler;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
