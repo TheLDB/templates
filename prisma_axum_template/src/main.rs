78df37c809a7361c9d6cabf5b5eae1b10909b89b
TheLDB <ldb@erikboles.com>
2022-09-01 22:11:17 -0600

    added prisma axum

--- prisma_axum_template/src/main.rs
+++ prisma_axum_template/src/main.rs
@@ -4,8 +4,24 @@ use std::net::SocketAddr;
 pub mod routes;
 use routes::handler::handler;
 
+mod prisma;
+use prisma::PrismaClient;
+
 #[tokio::main]
 async fn main() {
+    
+    dotenv::dotenv().ok();
+    // create prisma client
+    let client: PrismaClient = prisma::new_client().await.unwrap();
+
+    let test = client.users().find_many(vec![]).exec().await;
+    if let Ok(test) = test {
+        println!("{:?}", test);
+    }
+    else {
+        println!("issue")
+    }
+    
     // build our application with a route
     let app = Router::new().route("/", get(handler));
 
